1e20c42c1cf6b1f8796be0ae2aa3409e1d13274d
Dominic Egginton <dominic.egginton@gmail.com>
2022-03-23 17:26:17 +0000

    formula: update roll to 1.0.1

--- Formula/roll.rb
+++ Formula/roll.rb
@@ -1,8 +1,8 @@
 class Roll < Formula
   desc "Roll dice in the command line"
   homepage "https://github.com/dominicegginton/roll"
-  url "https://github.com/dominicegginton/roll/archive/refs/tags/1.0.0.tar.gz"
-  sha256 "044b5dfd06dc4a102f1448b8973197c24695fb3b9803df3eb94efc7c2e174042"
+  url "https://github.com/dominicegginton/roll/archive/refs/tags/1.0.1.tar.gz"
+  sha256 "0108ec5ea150ce628dd94e9a1b77b83383740e014ff203a0d6f5771fca799022"
   license "MIT"
 
   depends_on xcode: ["10.0", :build]
